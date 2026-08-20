02dfd72986bbeaef4410af6677c7db6689f3e83d
Everson Oliveira <everson.dev11@hotmail.com>
2026-08-20 01:17:32 -0300

    Adicionando o título do Swagger.

--- 01-Presentation/Program.cs
+++ 01-Presentation/Program.cs
@@ -1,10 +1,11 @@
-using System.Reflection;
 using _02_Application.Interfaces;
 using _02_Application.Services;
 using _03_Infrastructure.Data;
 using _03_Infrastructure.Repositories;
 using _04_Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,16 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
 {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Funcionários API",
+        Version = "v1",
+        Description = "API para gerenciamento de funcionários."
+    });
+
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
